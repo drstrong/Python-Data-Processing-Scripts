817efd854c38f87e552c3b13be2125252f337713
drstrong <strongprimemail@gmail.com>
2016-05-25 17:12:43 -0400

    Import Data
    Added an enum conversion using the status flags to save space.

--- MySQL_ImportData.py
+++ MySQL_ImportData.py
@@ -1,6 +1,6 @@
 #MySQL Import Data
 #By Eric Strong
-#Last modified: 2016/05/18
+#Last modified: 2016/05/25
 #
 #This program is meant to import CSV data into a MySQL database
 
@@ -8,23 +8,30 @@ import gzip
 import pandas
 from datetime import datetime
 from os import path, listdir
-from sqlalchemy import create_engine, MetaData, TEXT, Integer, Float, Table, Column
+from sqlalchemy import create_engine, MetaData, Integer, Float, Table, Column
+from sqlalchemy.dialects.mysql import TINYINT
 
 #INPUT PARAMETERS
 outputDirectory = r"C:\Users\estrong\Desktop\Anonymized Data"
+mapdict = {"OK":0,"UNRELIABLE":1,"OUT OF RANGE":2,"ALARM":3,"HIGH ALARM":4,
+           "LOW ALARM":5,"WARNING":6,"HIGH WARNING":7,
+           "LOW WARNING":8,"UNINITIALIZED":9,"NOT UPDATED":10,"CHATTERING":11,
+           "OK - SET":12,"OK - NOTSET":13}
 
 #START PROGRAM
 fileList = listdir(outputDirectory)
 startProgramTime = datetime.now();
 print("Program initialized at %s" % startProgramTime)
+
 #Create the tables for each file that will be imported
-engine = create_engine("mysql+pymysql://root:"+'1127Benfield'+"@localhost/test_db")
+engine = create_engine("mysql+pymysql://root:"+'1127Benfield'+"@localhost/test_db2")
 meta = MetaData(bind=engine)
 for filename in fileList:
     new_table = Table(filename.split(".")[0].lower(), meta,
     Column('DateTime', Integer, primary_key=True, autoincrement=False),
     Column('Value', Float, nullable=True),
-    Column('Status', TEXT, nullable=True)) 
+    Column('Status', TINYINT, nullable=True),
+    mysql_row_format='COMPRESSED') 
 meta.create_all(engine)
 
 #Add each data file to the database
@@ -42,6 +49,8 @@ for filename in fileList:
         df = pandas.read_csv(csvFile, index_col=0,header=None, names = ["DateTime","Value","Status"]) 
         #Remove the duplicates in the primary key "DateTime"
         df = df.groupby(df["DateTime"]).first()
+        #Create an enum from the status strings       
+        df["Status"] = df["Status"].apply(mapdict.get).astype(float)
         #Push to the db
         df.to_sql(filename.split(".")[0].lower(),engine,flavor='mysql', if_exists='append',index=True)
         #Print progress
